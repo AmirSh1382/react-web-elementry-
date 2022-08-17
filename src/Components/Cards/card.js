a006826db0b54425aaa2f2bb38ab91e39d15d133
AmirSh1382 <amirsh.webdev@gmail.com>
2022-08-17 18:39:30 +0430

    event handling added

--- src/Components/Cards/card.js
+++ src/Components/Cards/card.js
@@ -3,15 +3,43 @@ import React, { Component } from "react";
 import styles from "./card.module.css";
 
 class card extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  down1 = () => {
+    if (this.state.counter >= 1) {
+      this.setState(prevState => ({
+        counter: prevState.counter - 1,
+      }));
+    }
+  };
+
+  up1 = () => {
+    this.setState(prevState => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
   render() {
     const { name, price, src } = this.props;
+    const { counter } = this.state;
     return (
       <div className="col-12 col-md-6 col-lg-4 col-xl-3">
         <div className={styles.card}>
           <img src={src} alt="card" />
           <div>
             <h4 className="m-3">{name}</h4>
-            <span className="m-3 m-2 p-2">{price}$</span>
+            <span className="m-3 mb-0 p-2">{price}$</span>
+            <hr className="text-white m-0 mt-3" />
+            <div className={styles.counter}>
+              <i onClick={this.down1} className={this.state.counter ? "bi bi-arrow-down" : `${styles.deactive} bi bi-arrow-down`}></i>
+              <div>{counter}</div>
+              <i onClick={this.up1} className="bi bi-arrow-up"></i>
+            </div>
           </div>
         </div>
       </div>
--- src/Components/Search/search.js
+++ src/Components/Search/search.js
@@ -3,11 +3,28 @@ import React, { Component } from "react";
 import styles from "./search.module.css";
 
 class search extends Component {
+  constructor () {
+    super()
+    this.state = {
+      text: ""
+    }
+  }
+
+  changeHandler = event => {
+    this.setState({
+      text : event.target.value
+    })
+  }
+
   render() {
+    const { text } = this.state
     return (
       <div className={styles.container}>
         <div>What Are you looking for?</div>
-        <input placeholder="Search Sth..." />
+        <div>
+          <input placeholder="Search Sth..." onChange={this.changeHandler} value={text}/>
+          <div className={styles.inputValue}>{text}</div>
+        </div>
       </div>
     );
   }
